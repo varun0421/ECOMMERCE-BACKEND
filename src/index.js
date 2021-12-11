062e072dfaefd1b38e474ae3781fc36d4225913e
Sai Varun <karnati187@gmail.com>
2021-12-11 11:01:49 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 
-console.log("hi")
+app.get('/',(req,res)=>{
+    res.send(`Server listening at PORT ${port}`)
+})
 //route to handle auth requests
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category", categoryRoutes)
